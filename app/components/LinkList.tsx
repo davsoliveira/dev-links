81d87005bf499feac20a200f3aece106687108fb
Davi Oliveira <davi.ol.santos@gmail.com>
2025-11-13 15:51:30 -0300

    build: adicionar next-themes

--- app/components/LinkList.tsx
+++ app/components/LinkList.tsx
@@ -28,7 +28,7 @@ export function LinkList() {
         <li key={link.id}>
           <a
             href={link.href}
-            className="flex justify-center rounded-lg border border-white/40 bg-white/20 py-4 text-white shadow-[0_0_15px_rgba(255,255,255,0.2)] backdrop-blur-sm transition-all duration-300 text-shadow-[0_0_15px_rgba(255,255,255,1)] hover:bg-white/20"
+            className="flex justify-center rounded-lg border border-white/40 bg-white/10 py-4 backdrop-blur-sm transition-all duration-300 hover:bg-white/20"
           >
             {link.title}
           </a>
